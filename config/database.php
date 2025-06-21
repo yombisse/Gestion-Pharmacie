6869fe5f7b0e193c2edda583ed24a24f2c686f4e
yombisse <yombissefandie@gmail.com>
2025-06-21 10:19:07 +0000

    Fix Procfile for Railway

--- config/database.php
+++ config/database.php
@@ -41,15 +41,17 @@ return [
             'journal_mode' => null,
             'synchronous' => null,
         ],
+     
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            // 'url' => env('DB_URL'),  // <-- commente ou supprime cette ligne
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
+            // ... le reste 
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -59,7 +61,10 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                 PDO::ATTR_TIMEOUT => 60, 
             ]) : [],
+           
+
         ],
 
         'mariadb' => [
